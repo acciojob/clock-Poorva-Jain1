ad16674d5a68bd82cd0e3d325d55839831330759
Poorva-Jain1 <poorvajainnagod@gmail.com>
2025-09-07 22:33:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+ let timerDisplay = document.getElementById("timer");
+
+    // Function to update time
+    function updateTime() {
+      let currentDate = new Date(); // Get current date and time
+      timerDisplay.innerHTML = currentDate.toLocaleString(); // Display it
+    }
+
+    // Initial call
+    updateTime();
+
+    // Update the time every second
+    setInterval(updateTime, 1000);
